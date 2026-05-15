de1ed05ee88763344178c5a307600c10dd119222
Kohaku-Dev-AI <sen.kind.84@gmail.com>
2026-05-15 23:58:47 +0900

    Next Prime

---
+++ Easy100/021/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var x int
+	fmt.Scan(&x)
+
+	for {
+		if isPrime(x) {
+			fmt.Println(x)
+			break
+		}
+		x++
+	}
+}
