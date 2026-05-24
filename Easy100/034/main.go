4ab9a5d87a168b479e233cf6ee4a3ae92800710f
Kohaku-Dev-AI <sen.kind.84@gmail.com>
2026-05-24 16:44:06 +0900

    Not Found

---
+++ Easy100/034/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func nextString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	s := nextString()
+	alphabetMap := make(map[rune]bool)
+	for _, char := range s {
+		alphabetMap[char] = true
+	}
+
+	for i := 'a'; i <= 'z'; i++ {
+		if !alphabetMap[i] {
+			fmt.Println(string(i))
+			return
+		}
+	}
+	fmt.Println("None")
+}
